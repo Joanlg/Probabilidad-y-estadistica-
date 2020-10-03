6e44250825f829da19dc3aa0d77b9239473073d1
Joanlg <Joan_luna66@hotmail.com>
2020-10-03 17:47:58 -0500

    Me sentí bien 1

---
+++ Mi primer script.R
@@ -0,0 +1,6 @@
+#Vamos a crear un vector que contenga 5 variables cuantitativas deiscretas
+#contenida en una variable llamada muestra
+muestra<-c(10,35,20,18,15)
+#vamos a calcular la media
+mean(muestra)
+
